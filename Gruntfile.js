d7647016a0457f6a8ce803aadb1a9d6a414b35f7
Leo Lara <leo@leolara.me>
2014-11-18 01:30:33 +0100

    Update package.json

--- Gruntfile.js
+++ Gruntfile.js
@@ -544,7 +544,7 @@ module.exports = function ( grunt ) {
    * The `build` task gets your app ready to run for development and testing.
    */
   grunt.registerTask( 'build', [
-    'clean', 'html2js', 'jshint', 'coffeelint', 'coffee', 'less:build',
+    'clean', 'html2js', 'coffee', 'less:build',
     'concat:build_css', 'copy:build_app_assets', 'copy:build_vendor_assets',
     'copy:build_appjs', 'copy:build_vendorjs', 'index:build'
   ]);
